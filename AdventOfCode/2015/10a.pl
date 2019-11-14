785cdbd7e6b449b537605ec53abfe5400c88ad07
Furkan Usta <furkan.usta@bilkent.edu.tr>
2019-11-14 11:08:55 +0300

    Add AoC2015.10

---
+++ AdventOfCode/2015/10a.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+use autodie;
+use Data::Dumper;
+use v5.10; # for say
+
+my $input = "3113322113";
+
+for (1..40) {
+    my $str = '';
+    my $old = substr($input, 0, 1);
+    my $count = 0;
+    for my $ch(split //, $input) {
+        # say "| $ch | $old | $count | $str |";
+        if ($ch eq $old) {
+            $count++;
+        } else {
+            $str .= $count . $old;
+            $count = 1;
+        }
+        $old = $ch;
+    }
+    $input = $str . $count . substr($input, -1);
+}
+say length $input;
